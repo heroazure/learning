95769fa9c57fab423b8bf9751fc1656712c348a3
heroazure <1147024669@qq.com>
2016-05-29 22:58:58 +0800

    'webpack'

---
+++ webpackdemo/module1.js
@@ -0,0 +1,8 @@
+/**
+ * Created by heroxiao on 2016/5/27.
+ */
+'use strict';
+
+document.write('module1.js<br />')
+
+module.exports = '模块导出module1'
---
+++ webpackdemo/module2.js
@@ -0,0 +1,9 @@
+/**
+ * Created by heroxiao on 2016/5/27.
+ */
+'use strict';
+
+document.write('module2.js<br />')
+require('module3')
+
+module.exports = '模块导出module2'
